5eea2dd710102e7044bf73ca4441d15674220cfa
Kris <kristopher_anderson@icloud.com>
2018-03-05 14:38:36 +0000

    goodbye unnecessary console logs

--- public/places.js
+++ public/places.js
@@ -2,26 +2,13 @@ const Places = function() {
   this.pois = [];
   this.callback = this.callback.bind(this);
   this.displayPOIS = this.displayPOIS.bind(this);
-
 }
 
 
 
 Places.prototype.callback = function(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
-    //console.log(results.length);
-    //console.log(results);
-    //console.log(this);
     this.pois.push(results);
-  //  console.log(this.pois)
-    // if (this.pois.length > 5){
-    //   console.log(this.pois);
-    //   displayPOIS(this.pois);
-    // }
-    // for (var i = 0; i < results.length; i++) {
-    //   var place = results[i];
-    //   //console.log(place);
-    // }
   }
 }
 
@@ -32,7 +19,6 @@ Places.prototype.getGooglePlacesPOIs = function(latLong){
     const currentLocation = new google.maps.LatLng(latLong.lat, latLong.lng);
 
     poiTypes.forEach(function(type){
-    //  console.log(this);
       var request = {
         location: currentLocation,
         radius: '1000',
@@ -43,18 +29,13 @@ Places.prototype.getGooglePlacesPOIs = function(latLong){
       center: currentLocation,
       zoom: 15
     });
-      //console.log(googleMap);
       service = new google.maps.places.PlacesService(googleMap);
-      //console.log(this);
-      //console.log(service);
-      service.nearbySearch(request, this.callback);
-      //console.log(this);
-    }.bind(this));
-  //  console.log(this.pois);
-    this.displayPOIS();
-    //console.log(currentLocation);
 
-    //console.log(this.pois);
+      service.nearbySearch(request, this.callback);
+
+    }.bind(this));
+    this.displayPOIS();
+
 
 }
 
