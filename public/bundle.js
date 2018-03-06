80dc45d6ec0f7632209ba75e1cfdb221ecb32569
Dave Ellis <dave@ellisnetmail.co.uk>
2018-03-06 13:58:10 +0000

    Typo in keys.js

--- public/bundle.js
+++ public/bundle.js
@@ -13983,7 +13983,7 @@ document.addEventListener("DOMContentLoaded", appStart);
 
 const KEYS = {
    mapbox: 'pk.eyJ1IjoiZGF2ZXktZWxsaXMiLCJhIjoiY2plYmQydWo1MWVrdjJ5bzNmdmtiZXhhaiJ9.lNuXnwIwUmmdz82hHE-0Rg',
-   sigicTravel: 'jGGuMVeRWZ2x3ltjFRaj12sXfaxYpDzT4btQf2hV'
+   sygicTravel: 'jGGuMVeRWZ2x3ltjFRaj12sXfaxYpDzT4btQf2hV'
 }
 
 module.exports = KEYS;
--- public/keys.js
+++ public/keys.js
@@ -1,6 +1,6 @@
 const KEYS = {
    mapbox: 'pk.eyJ1IjoiZGF2ZXktZWxsaXMiLCJhIjoiY2plYmQydWo1MWVrdjJ5bzNmdmtiZXhhaiJ9.lNuXnwIwUmmdz82hHE-0Rg',
-   sigicTravel: 'jGGuMVeRWZ2x3ltjFRaj12sXfaxYpDzT4btQf2hV'
+   sygicTravel: 'jGGuMVeRWZ2x3ltjFRaj12sXfaxYpDzT4btQf2hV'
 }
 
 module.exports = KEYS;
