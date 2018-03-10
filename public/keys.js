7587d7e2290e48fb4476afd0997d97932d08c295
Kris <kristopher_anderson@icloud.com>
2018-03-10 11:59:47 +0000

    updated API Key for sygic

--- public/keys.js
+++ public/keys.js
@@ -1,6 +1,6 @@
 const KEYS = {
    mapbox: 'pk.eyJ1IjoiZGF2ZXktZWxsaXMiLCJhIjoiY2plYmQydWo1MWVrdjJ5bzNmdmtiZXhhaiJ9.lNuXnwIwUmmdz82hHE-0Rg',
-   sygicTravel: 'jGGuMVeRWZ2x3ltjFRaj12sXfaxYpDzT4btQf2hV',
+   sygicTravel: '9CJ6DmrF7c6hSLfnCSDfO4X4VLiVc1Mx2NyXJxRT',
    openWeather: '7d6069b54fdfea776a2da124a8152528'
 }
 
