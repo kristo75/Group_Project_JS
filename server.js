4c6ec388ab428632f71b63de8aa480883251a5cd
Kris <kristopher_anderson@icloud.com>
2018-03-20 10:14:13 +0000

    updated db connect

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const ObjectID = require('mongodb').ObjectID;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 console.log(process.env.MONGODB_URI)
-database.connect("mongodb://<admin>:<dbpassword>@ds261128.mlab.com:61128/heroku_rgv75lk1", function(error, client){
+const databaseUrl = process.env.MONGODB_URI || "mongodb://localhost:27017"
+database.connect(databaseUrl, function(error, client){
   if(error){
     console.log("Error:", error);
   return
